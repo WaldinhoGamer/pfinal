767965454658603f1aecccb48b173e189236ac3f
Waldair Otto <waldaotto@gmail.com>
2025-05-28 22:10:40 -0300

    atualizado

--- posicionar-naval.php
+++ posicionar-naval.php
@@ -2,36 +2,29 @@
 
 require "campos-naval.php";
 require "valorcoluna-naval.php";
+require "separaLetraNum.php";
 
-function inserirBarcos(&$campoPlayer,&$colun)
+function inserirBarcos(&$campoPlayer,&$colun,&$letra,&$numero)
 {
  for ($i=10; $i != 0; $i--) { 
     
-    $linha = readline("Insira a letra da linha do barco n° $i:");
-    $colun = readline("Insira o numero da coluna do barco n° $i:");
+    $posicao = readline("Insira a posição do barco:");
+
+
+   
 
     $colunis = substituirLetras($colun);
 
-    if($campoPlayer[$linha][$colunis] = "V"){
-        echo "Posição ja ocupada!";
-    }
-    else {
-
-   $campoPlayer[$linha][$colunis] = "V";
+    
+   $campoPlayer[$letra][$numero] = "V";
 
    exibeCampo($campoPlayer);
-    }
+    
  }
    
-// foreach($campoPlayer as $linha){
-//         foreach($linha as $item){
-//             echo "$item\t";
-//         }
-//         echo "\n";
-//     }
-//  }
+
 }
 $campoPlayer = campoPlayer();
 inserirBarcos($campoPlayer,$colun);
-// exibeCampo($campoPlayer);
+
 ?>
